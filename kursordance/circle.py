f913a8ace7f58f5c54a1ba4e8ac3d6aaad887630
SpookyBear0 <collinmcarroll@gmail.com>
2020-08-23 13:50:07 -0700

    scale image

--- kursordance/circle.py
+++ kursordance/circle.py
@@ -10,9 +10,10 @@ class Circle(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
-        img = pygame.image.load(path + "/assets/hitcircle.png")
+        img = pygame.image.load(path + "/assets/hitcircleoverlay.png")
         self.images.append(img)
         self.image = self.images[0]
         self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2 )
     def update(self):
         pass
